531cf4bc6310494f486c516afa9b3f6be7da37f1
Yusuf Solomon <ysolomon298@gmail.com>
2025-12-27 22:30:17 +0100

    had to change the yaml files to remove '_' in the names, also updated the service to enable internet facing and specify subnet ID

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ from PIL import Image
 import io
 import matplotlib.pyplot as plt
 
-url = "http://localhost:8000/predict"
+# url = "http://localhost:8000/predict"
+
+url = 'http://k8s-default-oilspill-9babf0751b-292ba6ab37ce3e10.elb.us-east-1.amazonaws.com:9696/predict'
 
 request = {
     "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQowQT72b_WbwMYsMACan4JLU64nWhVv0CxtA&s"
